ccea0bf5da25015f76a39ff3c391d89b8b3d123b
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-05 16:08:09 +0100

    set up of the middleware in order to validate the good datas received before to register a new repo with joi

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import data from "../../data.json";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import type { Repos } from "./repos.type";
+import Joi from "joi";
 
 const repos = express.Router();
 repos.get("/", (_, res: Response) => {
 	res.status(200).json(data);
 });
+
+const schema = Joi.object({
+	description: Joi.string().required(),
+	name: Joi.string().required(),
+	url: Joi.string().required(),
+});
+
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+	console.log(req.body);
+	const { error } = schema.validate(req.body);
+	if (error) {
+		res.status(422).json(error);
+	} else {
+		next();
+	}
+};
 
 repos.get("/:id", (req: Request, res: Response) => {
 	const repo = data.find((rep) => rep.id === Number(req.params.id)) as Repos;
@@ -18,7 +35,7 @@ repos.get("/:id", (req: Request, res: Response) => {
 	}
 });
 
-repos.post("/", (req: Request, res: Response) => {
+repos.post("/", validateRepo, (req: Request, res: Response) => {
 	const newRepo = { ...req.body, id: data.length + 1 };
 	data.push(newRepo);
 	res.status(201).json(newRepo);
