460c877542f3f24e8b89f1e5b04d13f7247b4580
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-05 12:28:29 +0100

    route api/repos is working and send datas

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import type { Response } from "express";
+import repos from "./repos/repos.controller";
 
 const router = express.Router();
 
 router.get("/", (_, res: Response) => {
 	res.status(200).send("Tout est ok !");
 });
+
+router.use("/repos", repos);
 
 export default router;
