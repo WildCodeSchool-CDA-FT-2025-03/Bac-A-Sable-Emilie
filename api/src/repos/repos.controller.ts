666ceb50d9504c80c3f7c4b6625da318c5bac944
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-06 10:54:43 +0100

    set up of a first filter in back end  on isPrivate if there is a req query of isPrivate received

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -5,8 +5,12 @@ import type { Repos } from "./repos.type";
 import { validateRepo } from "./repos.validate";
 
 const repos = express.Router();
-repos.get("/", (_, res: Response) => {
-	res.status(200).json(data);
+repos.get("/", (req: Request, res: Response) => {
+	console.log(req.query);
+	const result = req.query.isPrivate
+		? data.filter((rep) => rep.isPrivate.toString() === req.query.isPrivate)
+		: data;
+	res.status(200).json(result);
 });
 
 repos.get("/:id", (req: Request, res: Response) => {
